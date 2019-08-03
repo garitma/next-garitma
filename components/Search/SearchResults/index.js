d4daa040ebc69dee90afca4045fe0337553b11e3
Garitma <hola@garitma.com>
2019-08-03 18:59:28 -0500

    Fix error imagen

--- components/Search/SearchResults/index.js
+++ components/Search/SearchResults/index.js
@@ -10,16 +10,17 @@ export default class SearchResults extends React.Component {
                 <div className="page-search-module block">
                     <div className="page-search-wrapper coat">
                         <div className="search-thumbnail block small-4 medium-2">
-                            {document.data.featured_img
-                                ? <img className="responsive-image" src={document.data.featured_img.square.url} />
-                                : ``}
+
                         </div>
+
                         <div className="block small-8 medium-10 valign">
                             <div className="search-box-detail">
                                 <Link href="/[uid]" as={document.uid}>
                                     <h2 className="h5"><a>{RichText.asText(document.data.title)}</a></h2>
                                 </Link>
-                                <a className="button-link hide-small">Ver más</a>
+                                <Link href="/[uid]" as={document.uid}>
+                                    <a className="button-link hide-small">Ver más</a>
+                                </Link>
                             </div>
                         </div>
                     </div>
