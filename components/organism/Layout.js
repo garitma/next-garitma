f92c668d303d6b75ded8af83511776ed433ef31b
Garitma <hola@garitma.com>
2019-10-07 17:47:46 -0500

    PWA install

--- components/organism/Layout.js
+++ components/organism/Layout.js
@@ -22,13 +22,14 @@ export default class Layout extends React.Component {
             <Head>
                 <link rel='shortcut icon' sizes='16x16 24x24 32x32 48x48 64x64' href='/static/favicon.ico' />
                 <link rel='icon' type='image/x-icon' href='/static/favicon.ico' />
-                <link rel='apple-touch-icon' sizes='57x57' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=57&h=57' />
-                <link rel='apple-touch-icon-precomposed' sizes='57x57' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=57&h=57' />
-                <link rel='apple-touch-icon' sizes='72x72' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=72&h=72' />
-                <link rel='apple-touch-icon' sizes='114x114' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=114&h=114' />
-                <link rel='apple-touch-icon' sizes='120x120' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=120&h=120' />
-                <link rel='apple-touch-icon' sizes='144x144' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=144&h=144' />
-                <link rel='apple-touch-icon' sizes='152x152' href='https://images.prismic.io/garitma%2Fe357d4fe-1953-43e0-be0e-4f7f95e21fcc_favicon-192.png?auto=compress,format&w=152&h=152' />
+                <link rel='apple-touch-icon' sizes='57x57' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=57&h=57' />
+                <link rel='apple-touch-icon-precomposed' sizes='57x57' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=57&h=57' />
+                <link rel='apple-touch-icon' sizes='72x72' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=72&h=72' />
+                <link rel='apple-touch-icon' sizes='114x114' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=114&h=114' />
+                <link rel='apple-touch-icon' sizes='120x120' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=120&h=120' />
+                <link rel='apple-touch-icon' sizes='144x144' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=144&h=144' />
+                <link rel='apple-touch-icon' sizes='152x152' href='https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=152&h=152' />
+                <link rel="shortcut icon" href="https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format&w=512&h=512" />
                 <link href='https://auradesignsystem.com/static/style.css' rel='stylesheet' />
                 <link rel="manifest" href="static/manifest.json" />
                 <meta content='yes' name='apple-mobile-web-app-capable' />
@@ -37,7 +38,6 @@ export default class Layout extends React.Component {
                 <meta name="apple-mobile-web-app-title" content="Garitma" />
                 <meta name="apple-mobile-web-app-capable" content="no" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="transparent" />
-                <link rel="shortcut icon" href="https://images.prismic.io/garitma%2F7bcfd3a3-4da2-4b73-9402-612e7b20ce1c_favicon-pwa.png?auto=compress,format" />
                 <script src="//use.typekit.net/acc2awn.js"></script>
                 <script dangerouslySetInnerHTML={{ __html: `try{Typekit.load({ async: true });}catch(e){}` }} />
             </Head>
