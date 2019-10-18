048f39adde7e5c8394095e919273133fc56e2084
Garitma <hola@garitma.com>
2019-10-18 09:54:35 -0500

    Update search form style

--- components/atoms/MenuSearchForm.js
+++ components/atoms/MenuSearchForm.js
@@ -3,8 +3,8 @@ export default class MenuSearchForm extends React.Component {
         const { value } = this.props
         return (
             <form role="search" method="get" id="searchform" className="menu-item" action="/resultados">
-                <input aria-label="Buscar en Garitma" style={{ border: 0, fontWeight: 700, paddingLeft: `45px` }} id="search" type="search" placeholder="" name="s" required />
-                <div style={{ left: `15px` }} className="glyphsSprite search action left disable"></div>
+                <input aria-label="Buscar en Garitma" id="search" type="search" placeholder="" name="s" required />
+                <div className="glyphsSprite search action left-1 disable"></div>
             </form>
         )
     }
--- components/atoms/SearchForm.js
+++ components/atoms/SearchForm.js
@@ -5,7 +5,7 @@ export default class SearchForm extends React.Component {
             <form role="search" method="get" id="searchformresults" className="layer" action="/resultados">
                 <div className="halo inputer">
                     <input aria-label="Buscar en Garitma" className="typeahead layer" id="search" type="search" placeholder="Buscar en Garitma" name="s" required />
-                    <div className="glyphsSprite search action left disable"></div>
+                    <div className="glyphsSprite search action left-4 disable"></div>
                 </div>
             </form>
         )
