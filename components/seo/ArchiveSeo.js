d7f6978236b40cec6e1c3d33e53b1c73d58d5d7e
Pablo Orozco Montes <hola@garitma.com>
2020-02-12 21:08:21 -0500

    Fix seo folder bug

---
+++ components/seo/ArchiveSeo.js
@@ -0,0 +1,41 @@
+import NextSeo from 'next-seo'
+import GaritmicConfig from '../../garitmic.config.json'
+
+export default class ArchiveSeo extends React.Component {
+    render() {
+        const { document } = this.props
+        return (
+            <NextSeo
+                config={{
+                    title: `${GaritmicConfig.types[document.results[0].type].name}`,
+                    titleTemplate: `%s | ${GaritmicConfig.siteName}`,
+                    description: `Encuentra más de ${document.total_results_size} ${GaritmicConfig.types[document.results[0].type].type} de amor, desamor y cotidianos creado por Garitma.`,
+                    canonical: `${GaritmicConfig.siteURL}/categorias/${document.results[0].type}`,
+                    openGraph: {
+                        url: `${GaritmicConfig.siteURL}/categorias/${document.results[0].type}`,
+                        title: `${GaritmicConfig.types[document.results[0].type].name} | ${GaritmicConfig.siteName}`,
+                        description: `Encuentra más de ${document.total_results_size} ${GaritmicConfig.types[document.results[0].type].type} de amor, desamor y cotidianos creado por Garitma.`,
+                        type: 'Website',
+                        images: [
+                            {
+                                url: `${GaritmicConfig.social.openGrap}`,
+                                width: 1140,
+                                height: 570,
+                                alt: 'Open Graph logo Garitma',
+                            },
+                        ],
+                        site_name: `${GaritmicConfig.siteName}`,
+                    },
+                    twitter: {
+                        handle: `@${GaritmicConfig.social.twitter}`,
+                        site: `@${GaritmicConfig.social.twitter}`,
+                        cardType: 'summary_large_image',
+                    },
+                    facebook: {
+                        appId: `${GaritmicConfig.social.facebookappId}`
+                    }
+                }}
+            />
+        )
+    }
+}
---
+++ components/seo/GeneralSeo.js
@@ -0,0 +1,40 @@
+import NextSeo from 'next-seo'
+import GaritmicConfig from '../../garitmic.config.json'
+
+export default class GeneralSeo extends React.Component {
+    render() {
+        return (
+            <NextSeo
+                config={{
+                    title: `${GaritmicConfig.siteName}`,
+                    titleTemplate: `%s`,
+                    description: `${GaritmicConfig.siteDescription}`,
+                    canonical: `${GaritmicConfig.siteURL}`,
+                    openGraph: {
+                        url: `${GaritmicConfig.siteURL}`,
+                        title: `${GaritmicConfig.siteName}`,
+                        description: `${GaritmicConfig.siteDescription}`,
+                        type: 'Website',
+                        images: [
+                            {
+                                url: `${GaritmicConfig.social.openGrap}`,
+                                width: 1140,
+                                height: 570,
+                                alt: 'Open Graph logo Garitma',
+                            },
+                        ],
+                        site_name: `${GaritmicConfig.siteName}`,
+                    },
+                    twitter: {
+                        handle: `@${GaritmicConfig.social.twitter}`,
+                        site: `@${GaritmicConfig.social.twitter}`,
+                        cardType: 'summary_large_image',
+                    },
+                    facebook: {
+                        appId: `${GaritmicConfig.social.facebookappId}`
+                    }
+                }}
+            />
+        )
+    }
+}
