1985ebd6a3eea9182ed7483a0e3fae6a8044ceef
Pablo Orozco Montes <hola@garitma.com>
2020-02-25 20:25:38 -0500

    Update style media player er

--- components/templates/SingleRead.js
+++ components/templates/SingleRead.js
@@ -14,16 +14,14 @@ export default class SingleRead extends React.Component {
 
         return (
             <article className="post">
-                <div className="feature" itemScope itemType="http://schema.org/CreativeWork" style={{ backgroundColor: `${document.data.color}` }}>
+                <div className="feature" itemScope itemType="http://schema.org/CreativeWork">
                     <SingleSeo document={document} />
                     <div className="smush" >
                         <div className="mod-media" >
                             <img alt={document.data.featured_img.alt} srcSet={`${document.data.featured_img.url}&w=640&h=320&dpr=1&fit=crop 640w,${document.data.featured_img.url}&w=750&h=375&dpr=1&fit=crop 750w, ${document.data.featured_img.url}&w=1080&h=540&dpr=1&fit=crop 1080w`} src={`${document.data.featured_img.url}&w=1140&h=570&dpr=1&fit=crop`} />
                             <SingleAction document={document} />
                         </div>
-                        
                     </div>
-                    
                 </div>
                 <div className="pad centertxt">
                     {archive ?
@@ -35,12 +33,15 @@ export default class SingleRead extends React.Component {
                         {document.type != 'podcasts' &&
                             <h1 itemProp="name">{RichText.asText(document.data.title)}</h1>
                         }
-                        
                     </div>
                     <div className='glyphsSprite logo' />
                     <div className="h6"><span itemProp="author">Garitma</span> · <time>{moment(document.data.date).locale(`${GaritmicConfig.lang}`).format(`${GaritmicConfig.dateFormat}`)}</time></div>
                     <div className="content smash pad h4 light">
-                        {RichText.render(document.data.content)}
+                        {document.type != 'podcasts' &&
+                            <>
+                                {RichText.render(document.data.content)}
+                            </>
+                        }
                         <div data-wio-id={document.id}></div>
                     </div>
                     <div className="h6">
@@ -51,6 +52,8 @@ export default class SingleRead extends React.Component {
                         }
                     </div>
                 </div>
+
+
                 {document.type == 'videos' &&
                     <style jsx>{`
                         .feature {
@@ -59,18 +62,11 @@ export default class SingleRead extends React.Component {
                       
                   `}</style>
                 }
-                {document.type == 'podcasts' &&
+
+                {document.data.color &&
                     <style jsx>{`
-                        .bxqrcA {
+                        .feature, .bxqrcA {
                             background-color: ${document.data.color};
-                            border: 0;
-                            display: block
-                        }
-                        .igagJy h4 {
-                            margin: 0 0 13px 0
-                        }
-                        .dLjWml {
-                            background-color: #212121
                         }
                   `}</style>
                 }
