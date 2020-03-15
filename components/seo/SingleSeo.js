564fdbdfb8a9331fe241db6d93467f22e1977606
Pablo Orozco Montes <hola@garitma.com>
2020-03-15 18:34:33 -0500

    Crop feature imagen single

--- components/seo/SingleSeo.js
+++ components/seo/SingleSeo.js
@@ -21,7 +21,7 @@ export default class SingleSeo extends React.Component {
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content={`${RichText.asText(document.data.title)} | ${GaritmicConfig.siteName}`} />
                 <meta property="og:description" content={RichText.asText(document.data.excerpt)} />
-                <meta property="og:image" content={document.data.featured_img.url} />
+                <meta property="og:image" content={`${document.data.featured_img.url}&w=1140&h=570&dpr=1&fit=crop`} />
                 <meta property="og:image:alt" content={document.data.featured_img.alt} />
                 <meta property="og:image:width" content="1140" />
                 <meta property="og:image:height" content="570" />
