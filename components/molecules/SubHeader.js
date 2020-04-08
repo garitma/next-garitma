73a5c2cff58ebd98ca6f2b333d184505b01d1e63
Pablo Orozco Montes <hola@garitma.com>
2020-04-08 06:34:04 -0500

    Update sub menu validation text

--- components/molecules/SubHeader.js
+++ components/molecules/SubHeader.js
@@ -13,24 +13,12 @@ export default class SubHeader extends React.Component {
                         <h3  className='content-center light centertxt mb0'>{plaintxt}</h3> 
                     }
                     <ul className="nav-list flowx wall-pad">
-                        {text != 'Poemas' &&
-                            <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/poemas'><a className="menu-item">Poemas</a></Link></li>
-                        }
-                        {text != 'Frases' &&
-                            <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/frases'><a className="menu-item">Frases</a></Link></li>
-                        }
-                        {text != 'Cómics' &&
+                        <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/poemas'><a className="menu-item">Poemas</a></Link></li>
+                        <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/frases'><a className="menu-item">Frases</a></Link></li>
                         <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/comics'><a className="menu-item">Cómics</a></Link></li>
-                        }
-                        {text != 'Videos' && 
                         <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/videos'><a className="menu-item">Videos</a></Link></li>
-                        }
-                        {text != 'Descargas' &&
                         <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/descargas'><a className="menu-item">Descargas</a></Link></li>
-                        }
-                        {text != 'Podcasts' &&
                         <li onClick={onClose}><Link href='/categorias/[type]' as='/categorias/podcasts'><a className="menu-item">Podcasts</a></Link></li>
-                        }
                     </ul>
                     
                 </div>
