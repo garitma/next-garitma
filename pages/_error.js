e4c71b99e5f15b3680bb414c716cfd44bb4dcfa3
Pablo Orozco Montes <me@pablopvsky.com>
2021-05-25 22:32:54 -0500

    _error js page

---
+++ pages/_error.js
@@ -0,0 +1,41 @@
+import Section from "aura-design/section";
+import Button from "aura-design/button";
+import Head from "next/head";
+import Link from "next/link";
+
+const MyError = ({ statusCode }) => {
+  return (
+    <Section container="smush">
+      <Head>
+        <title>Error {statusCode} | Garitma</title>
+      </Head>
+      <div className="aureole two valign" style={{ minHeight: "85vh" }}>
+        <div className="one valign">
+          <div className="smosh">
+            <h1 className="centertxt">
+              {statusCode === 404
+                ? " 404 página no encontrada"
+                : `${statusCode} ha ocurrido un problema`}
+            </h1>
+            {statusCode === 404 && (
+              <div className="aureole one">
+                <Link href="/">
+                  <Button mode="link">Volver al inicio</Button>
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="valign centertxt mod-media">
+          <img
+            width="500"
+            src="https://media.giphy.com/media/1rPoJNeGNTIUuFFnqK/giphy.gif"
+            className="container halo"
+          />
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default MyError;
