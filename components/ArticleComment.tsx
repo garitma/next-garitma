7b76351fa9c1565fb916842287dcb4ddf5caa1dc
Pablo Orozco Montes <me@pablopvsky.com>
2021-10-20 13:42:13 -0500

    Declare window render cusdis

--- components/ArticleComment.tsx
+++ components/ArticleComment.tsx
@@ -2,6 +2,12 @@ import { useEffect, useRef } from "react";
 import Section from "aura-design/section";
 import { useScript } from "@utils/script";
 
+declare global {
+  interface Window {
+    renderCusdis: any;
+  }
+}
+
 const ArticleComment = ({ title, id, path }) => {
   const { loaded, error } = useScript("https://cusdis.com/js/cusdis.es.js");
   useScript("https://cusdis.com/js/widget/lang/es.js");
@@ -19,16 +25,16 @@ const ArticleComment = ({ title, id, path }) => {
   }
 
   return (
-      <div
-        id="cusdis_thread"
-        data-host="https://cusdis.com"
-        data-app-id="29de8be7-c3b2-4a70-8554-2e32de338327"
-        data-page-id={id}
-        data-page-url={path}
-        data-page-title={title}
-        ref={cudis}
-        data-theme="lightc"
-      />
+    <div
+      id="cusdis_thread"
+      data-host="https://cusdis.com"
+      data-app-id="29de8be7-c3b2-4a70-8554-2e32de338327"
+      data-page-id={id}
+      data-page-url={path}
+      data-page-title={title}
+      ref={cudis}
+      data-theme="lightc"
+    />
   );
 };
 
