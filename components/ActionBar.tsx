fecc2c388ff5db4c1c3279bd791d6028d06d0f7a
Pablo Orozco <me@pablopvsky.com>
2022-09-18 10:38:17 -0500

    Change action bar

--- components/ActionBar.tsx
+++ components/ActionBar.tsx
@@ -6,7 +6,7 @@ const ActionBar = () => {
       <div className="smush">
         <p className="mb0 mt0 centertxt">
           <span className="wall-pad info-text info">Nuevo</span> • Ya está
-          disponible en Colombia 🇨🇴 mi tienda en linea de cuadernos Garitmática.
+          disponible en Colombia Garitmática, la tienda oficial de Gartima 🇨🇴
           <Button mode="link" href="https://garitmatica.com" target="_blank">
             Conocer
           </Button>
