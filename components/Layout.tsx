2c7879cf686a7ba30b863239fe8d83eeb7e295bd
Pablo Orozco Montes <me@pablopvsky.com>
2023-10-16 16:44:34 -0500

    Add meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,17 @@
 import Header from "@/components/Header";
+import Meta from "@/components/Meta";
+import type { MetaProps } from "@/components/Meta";
 
-const Layout = ({ children }) => {
+type LayoutProps = {
+  children: React.ReactNode;
+  seo?: MetaProps;
+};
+
+const Layout = ({ children, seo }: LayoutProps) => {
   return (
     <main>
       <div className="page-pancake">
-        {/* <Meta {...seo} /> */}
+        <Meta {...seo} />
         <Header />
         <div>{children}</div>
         {/* <Footer footer={footer} /> */}
