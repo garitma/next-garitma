9611db6774c95156a1d5e7fe74da9539557485cc
Pablo Orozco Montes <me@pablopvsky.com>
2023-10-16 22:16:06 -0500

    Add elevate Intro

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from "next/app";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
 import "@aura-design/system/main.css";
 
 import "@/styles/main.css";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PrismicPreview repositoryName={repositoryName}>
+      <Component {...pageProps} />
+    </PrismicPreview>
+  );
 }
 
 export default MyApp;
